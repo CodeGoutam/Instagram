16295238c49fd2d4553d216b1af40a5302f7f06b
Hemant Goutam <hgoutam2001@gmail.com>
2025-06-15 14:08:33 +0530

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,69 @@
+"use client";
+import { Box, Button, Input, Typography } from "@mui/material";
+import React from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Image from "next/image";
+import * as styles from "./styles";
+
+const LoginPage = () => {
+  return (
+    <Box sx={styles.wrapper}>
+      <Box sx={styles.card}>
+        <Typography sx={styles.language}>
+          English (UK)
+          <KeyboardArrowDownIcon />
+        </Typography>
+
+        <div style={styles.content}>
+          <Box sx={styles.logoBox}>
+            <Image
+              src={"/assets/instagram.png"}
+              height={40}
+              width={40}
+              alt="Instagram Logo"
+            />
+          </Box>
+          <Box sx={styles.formSection}>
+            <form style={styles.form}>
+              <div style={styles.inputWrapper}>
+                <Input
+                  type="text"
+                  disableUnderline
+                  placeholder="Username,email address or mobile number"
+                  sx={styles.input}
+                />
+                <Input
+                  type="password"
+                  disableUnderline
+                  placeholder="Password"
+                  sx={styles.input}
+                />
+              </div>
+              <Button type="submit" style={styles.loginButton}>
+                Log In
+              </Button>
+            </form>
+            <Typography sx={styles.forgotText}>Forgotten password?</Typography>
+          </Box>
+
+          <Box sx={styles.footer}>
+            <Button variant="outlined" type="submit" style={styles.createBtn}>
+              Create new account
+            </Button>
+            <Box sx={styles.metaBox}>
+              <Image
+                src={"/assets/meta.png"}
+                height={16}
+                width={16}
+                alt="meta Logo"
+              />
+              <Typography sx={styles.metaText}>Meta</Typography>
+            </Box>
+          </Box>
+        </div>
+      </Box>
+    </Box>
+  );
+};
+
+export default LoginPage;
---
+++ src/app/styles.ts
@@ -0,0 +1,126 @@
+import { SxProps, Theme } from "@mui/material";
+
+export const wrapper: SxProps<Theme> = {
+  width: "100vw",
+  height: "100vh",
+  display: "flex",
+  justifyContent: "center",
+  backgroundColor: "#fafafa",
+};
+
+export const card: SxProps<Theme> = {
+  width: "100%",
+  maxWidth: { sm: "100%", md: "350px" },
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+  padding: "20px",
+  borderRadius: "2px",
+  flexDirection: "column",
+  height: "100vh",
+  backgroundColor: "#FFFFFF",
+};
+
+export const language: SxProps<Theme> = {
+  fontSize: "14px",
+  color: "#8e8e8e",
+  fontWeight: 600,
+};
+
+export const content = {
+  width: "100%",
+  display: "flex",
+  flexDirection: "column",
+  gap: "32px",
+  alignItems: "center",
+  height: "100%",
+} as const;
+
+export const logoBox: SxProps<Theme> = {
+  height: "50%",
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+};
+
+export const formSection: SxProps<Theme> = {
+  width: "100%",
+  height: "50%",
+  display: "flex",
+  flexDirection: "column",
+  gap: "14px",
+};
+
+export const form = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "8px",
+} as const;
+
+export const inputWrapper = {
+  display: "flex",
+  flexDirection: "column",
+  gap: "8px",
+  width: "100%",
+} as const;
+
+export const input: SxProps<Theme> = {
+  border: "1px solid gray",
+  padding: "10px",
+  borderRadius: "12px",
+  height: "48px",
+  fontSize: "14px",
+  width: "100%",
+  fontWeight: "lighter",
+};
+
+export const loginButton = {
+  backgroundColor: "#4a5df9",
+  color: "white",
+  borderRadius: "16px",
+  border: "none",
+  cursor: "pointer",
+  height: "40px",
+} as const;
+
+export const forgotText: SxProps<Theme> = {
+  fontSize: "12px",
+  color: "#8e8e8e",
+  fontWeight: 600,
+  textAlign: "center",
+};
+
+export const footer: SxProps<Theme> = {
+  width: "100%",
+  textAlign: "center",
+  height: "50%",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "end",
+  gap: "8px",
+};
+
+export const createBtn = {
+  borderRadius: "16px",
+  border: "2px solid #4a5df9",
+  cursor: "pointer",
+  height: "40px",
+  width: "100%",
+  textTransform: "none",
+  color: "#4a5df9",
+  fontSize: "16px",
+} as const;
+
+export const metaBox: SxProps<Theme> = {
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  gap: "8px",
+};
+
+export const metaText: SxProps<Theme> = {
+  fontSize: "14px",
+  color: "#8e8e8e",
+  fontWeight: 500,
+  textAlign: "center",
+};
