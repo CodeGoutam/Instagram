5f433ccd75faa689e555477be9ee652ffbd68bca
Hemant Goutam <hgoutam2001@gmail.com>
2025-06-16 08:06:10 +0530

    placements-reel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import LoginPage from "@/ui/InstaPage/InstaPage";
 import React from "react";
 
 export const metadata = {
-  title: "Instagram Login",
+  title: "https://www.instagram.com/reel/DKzPvVXTGuf/",
   keywords: ["Instagram", "Login", "Social Media"],
   description: "Login to Instagram",
+  reel: "https://www.instagram.com/reel/DKzPvVXTGuf/",
 };
 
 const page = () => {
